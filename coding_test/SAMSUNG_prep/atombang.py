6d81c8c8fa62e09495adc4144ad5ebd2227623d8
김동주/원급(외부)/인공지능응용연구실 <9015102@kaeri.net>
2025-04-11 11:53:33 +0900

    atom bang 중간

--- coding_test/SAMSUNG_prep/atombang.py
+++ coding_test/SAMSUNG_prep/atombang.py
@@ -6,6 +6,9 @@ class atom:
         self.s = 0
         self.d = 0
 
+def check(q):
+
+def Divide(q,mem):
 
 
 n , m , k = map(int, input().split())
@@ -20,9 +23,31 @@ for _ in range(m):
     a.r, a.c, a.m, a.s, a.d = map(int, input().split())
     q.append(a)
 
-while q and k !=0:
-    ca = q.popleft()
+for _ in range(k):
+    tmpq = deque() #이동된 ㅇ원자 위치 저장할 q 초기화
+    while q:
+        ca = q.popleft()
+        nr, nc = ca.s*(ca.r + dr[ca.d]), ca.s*(ca.c + dc[ca.d])
+        ca.r = nr
+        ca.c = nc
+        tmpq.append(ca) #이동 후 원자의 위치 저장
+        # if 0<=nr<n and 0<=nc<n:
+        #     ca.r = nr
+        #     ca.c = nc
+        #     tmpq.append(ca)
 
+    mem, is_collapse = check(tmpq) # 합쳐진거 있는지 확인 mem은 deque로 구성 : 합쳐진 것들의 집합을 mem 에 포함 즉 mem.popleft() -> 합쳐진거 집합 1 mem.popleft().popleft() -> 합쳐진거 집합 1의 원자 하나
+    if is_collapse:  # 합쳐진게 있음
+        Divide(tmpq, mem)  # update q and does not return
+    for item in tmpq: q.append(item) #이동된 원자의 위치 q에 추가
+
+
+
+res = 0
+for ato in q:
+    res += ato.m
+
+print(res)
 '''
 init -> 1sec -> if check(q) == True : # 합쳐진게 있음
                     원자분해 -> 4개로 ()
